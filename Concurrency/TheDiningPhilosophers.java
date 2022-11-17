6070c66bee7ea78fa87d605a82394dd6eb9ac807
UNDERHMA <44343256+UNDERHMA@users.noreply.github.com>
2022-11-16 20:09:29 -0500

    Writing faster, shorter code
    Writing faster, shorter code

--- Concurrency/TheDiningPhilosophers.java
+++ Concurrency/TheDiningPhilosophers.java
@@ -2,64 +2,35 @@ package leetcode.LeetCode;
 
 public class TheDiningPhilosophers {
 	
-	private boolean[] forkLock = new boolean[] {false, false, false, false, false};
+	private boolean[] forks = new boolean[5];
 
 	public TheDiningPhilosophers() { 
     }
 
     // call the run() method of any runnable to execute its code
-    public void wantsToEat(int philosopher,
+    public synchronized void wantsToEat(int philosopher,
                            Runnable pickLeftFork,
                            Runnable pickRightFork,
                            Runnable eat,
                            Runnable putLeftFork,
                            Runnable putRightFork) throws InterruptedException {
-    	
-    	synchronized(this) {
-    		if(philosopher == 0 && forkLock[philosopher] == true || 
-    				forkLock[4] == true) {
-    			wait();
-    		}
-    		else if(philosopher != 0 && (forkLock[philosopher] == true 
-    				|| forkLock[philosopher-1] == true)) {
-    			wait();
-    		}
-    		
-    		if(philosopher == 0) {
-    			forkLock[4] = true;
-    			forkLock[1] = true;
-    		}
-    		else if(philosopher == 4) {
-    			forkLock[0] = true;
-    			forkLock[3] = true;
-    		}
-    		else {
-    			forkLock[philosopher+1] = true;
-    			forkLock[philosopher-1] = true;
-    		}
-    		notifyAll();
-    		
-    		// Perform eating
-        	pickLeftFork.run();
-        	pickRightFork.run();
-        	eat.run();
-        	putLeftFork.run();
-        	putRightFork.run();
-        	
-        	// Unlock
-        	if(philosopher == 0) {
-    			forkLock[4] = false;
-    			forkLock[1] = false;
-    		}
-    		else if(philosopher == 4) {
-    			forkLock[0] = false;
-    			forkLock[3] = false;
-    		}
-    		else {
-    			forkLock[philosopher+1] = false;
-    			forkLock[philosopher-1] = false;
-    		}
-    		notifyAll();
-    	}
+        
+        int left = philosopher == 0 ? 4 : philosopher-1;
+        int right = philosopher == 4 ? 0 : philosopher+1;
+        while(forks[left] || forks[right]) {
+            try {
+                wait();
+            } catch (InterruptedException ie) {}
+        }
+        forks[left] = true;
+        forks[right] = true;
+        pickLeftFork.run();
+        pickRightFork.run();
+        eat.run();
+        putLeftFork.run();
+        putRightFork.run();
+        forks[left] = false;
+        forks[right] = false;
+        notifyAll();
     }
 }
